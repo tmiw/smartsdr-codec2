edf63f138654fc952362816a46e070112a0cabbd
Annaliese McDermond <nh6z@nh6z.net>
2019-11-05 20:54:00 -0800

    Remodel Build System
    Remodel the build system to hopefully make it easier to create ARM
    binaries for the radio.  Included in here are some toolchain
    configuration files for crosstools-ng and modifications to CMake to
    make it cross compile for you.  There are now two extra CMake build
    types to use, ARMDebug and ARMRelease.  They will build their respective
    traditional build types only for ARM.  You will need to make sure you
    have the proper tools built by the included crosstools-ng configuration
    in your path before executing CMake.  There will be instructions later
    detailing how things are built.
    
    Signed-off-by: Annaliese McDermond <nh6z@nh6z.net>

--- DSP_API/cmake-toolchains/flex6k.cmake
+++ DSP_API/cmake-toolchains/flex6k.cmake
@@ -1,13 +1,3 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-
-#set(CMAKE_SYSROOT /home/devel/rasp-pi-rootfs)
-#set(CMAKE_STAGING_PREFIX /home/devel/stage)
-
-set(tools /home/mcdermj/x-tools/arm-linux-gnueabi)
-set(CMAKE_C_COMPILER ${tools}/bin/flex6k-gcc)
-
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_C_COMPILER flex6k-gcc)
